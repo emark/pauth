f8f5cd84cb77c0ad7ebb2a802695f1c1584fea29
Maxim Sviridenko <maxim@emrk.ru>
2016-05-03 22:05:15 +0700

    edit SQL schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 create database pauth;
 use pauth;
 create table clients (id integer primary key auto_increment, cdate datetime, phone varchar(10), ip varchar(15), mac varchar(17), code integer);
-create table notify_q(id integer primary key auto_increment, cid integer, result text);
-create table rules_q (id integer auto_increment primary key, cid integer, result boolean);
+create table notify_q(id integer primary key auto_increment, token text, result text);
+create table rules_q (id integer primary key auto_increment, cid integer, result text);

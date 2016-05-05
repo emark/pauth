da531cc52b8e2cfddc5585fdf018e2182780f99f
Maxim Sviridenko <maxim@emrk.ru>
2016-05-05 23:00:22 +0700

    bugfix

--- auth.pl
+++ auth.pl
@@ -73,7 +73,7 @@ sub register(){
 	my $token = ''; #Token uses for identification clients id
 	my @dict = (0..9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 
-	for (my $i=0; $i<=30; $i++){
+	for (my $i=0; $i<30; $i++){
     	$token = $token.$dict[rand(61)];
     };
 

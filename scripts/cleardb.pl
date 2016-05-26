dcdcc3b47bbc39ed457bd08b1dae2569ae8a4bf5
Maxim Sviridenko <maxim@emrk.ru>
2016-05-27 00:17:07 +0700

    bugfix

--- scripts/cleardb.pl
+++ scripts/cleardb.pl
@@ -22,7 +22,7 @@ my $past_date = &sql_date_format;
 #Clear database from empty mac address
 $dbi->delete(
 	table => 'clients',
-	where => "cdate<\"$past_date\" and mac=\"0\"",
+	where => "cdate < \"$past_date\" and mac = \"0\"",
 );
 
 &archiving if($task eq 'archiving');
@@ -31,7 +31,7 @@ sub archiving(){
 	my $hosts = $dbi->select(
 		table => 'clients',
 		column => ['cdate','phone','ip','mac'],
-		where => 'mac is not null',
+		where => 'mac <> 0',
 	)->fetch_hash_all;
 
 	foreach my $host(@{$hosts}){

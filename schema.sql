c342677064ea7202cb96c3688ad6428a87e8390a
Maxim Sviridenko <maxim@emrk.ru>
2016-08-15 22:02:11 +0700

    add case sensitive

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-create database pauth;
+create database pauth default character set utf8 collate utf8_bin;
 use pauth;
 create table clients (id integer primary key auto_increment, cdate datetime, phone varchar(10), ip varchar(15), mac varchar(17) default 0, code integer, token varchar(30));
 create table notify_q(id integer primary key auto_increment, token text, result text);

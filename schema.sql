20089dc22f10cbe9bd0f25138d05efb739232797
Maxim Sviridenko <maxim@emrk.ru>
2020-12-29 08:07:59 +0700

    add sql statement

--- schema.sql
+++ schema.sql
@@ -4,5 +4,6 @@ use pauth;
 create table clients (id integer primary key auto_increment, cdate datetime, phone varchar(10), ip varchar(15), mac varchar(17) default 0, code integer, token varchar(30));
 create table notify_q(id integer primary key auto_increment, token text, result text);
 create table rules_q (id integer primary key auto_increment, token text, result int default 0);
-create table hosts (id integer primary key auto_increment,cdate datetime,phone varchar(10),ip varchar(15), mac varchar(17));
+create table hosts (id integer primary key auto_increment, cdate datetime, phone varchar(10), ip varchar(15), mac varchar(17));
+create table routers (id integer primary key auto_increment, parent integer default 0, ip varchar(15), status tinyint(1) default 0, updated datetime, caption char(50));
 grant all on pauth.* to pauth@localhost identified by 'awake247';
